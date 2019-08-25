791f9772943b1ad49b70021ed9de513c72cc2047
ahmad baydowi <ahmadbaydowi88@gmail.com>
2019-08-25 12:45:14 +0700

    cuma 1 conditional bang

---
+++ belajar.js
@@ -0,0 +1,12 @@
+var gajiku = 500;
+var gajimu = 1000;
+
+if (gajiku > gajimu) {
+    console.log("Gaji gua lebih besar dari gaji lu");
+}
+else if (gajiku < gajimu) {
+    console.log("Gaji gua lebih kecil dari gaji lu");
+}
+else if (gajiku == gajimu) {
+    console.log("Gaji gua sama aja dengan gaji lu");
+}
--- conditional.js
+++ conditional.js
@@ -32,4 +32,42 @@ if (1 > 0) {
     }
     console.log('yang 2 > 1 bisa jadi salah bro');
 }
-828
+
+var Umur = 18;
+if (Umur > 17) {
+    console.log("Saya harus mempunyai KTP");
+}
+console.log(" dan layak  menpunyai SIM");
+
+var angka = 17;
+if (angka % 2 == 0) {
+    console.log("Angka = " + angka);
+    console.log("Angka adalah bilangan genap");
+}
+else {
+    console.log("Angka = " + angka);
+    console.log("Angka adalah bilangan ganjil");
+}
+
+var h = new Date();
+var mingguan = new Array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu");
+console.log("Hari : " + mingguan[h.getDay()]);
+var hari = h.getDay();
+if (hari == 0 || hari == 6) {
+    console.log("Selamat Berlibur");
+}
+else if (hari == 1) {
+    console.log("I Like Monday");
+    console.log("Selamat Bekerja");
+}
+else {
+    console.log("Selamat Bekerja");
+}
+var angka = 7;
+
+if (angka > 5) {
+    console.log("angka ini lebih besar dari 5");
+    if (angka <= 10) {
+        console.log("angka ini lebih besar atau sama dengan 10");
+    }
+}
